f58597ce26a121070525cab5e0fa15df5bf1a690
Hager yehia abdallah <hageryehia10@gmail.com>
2024-09-06 19:56:42 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-body: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    Center(
-      child: Image.asset(
-        'assests/Untitled design.png',
-        height: 200,
-        width: 200,
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 70),
+                    child: Image.asset('assets/Assignment.png',
+                        height: 250, width: 450),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text('Welcome to the app',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.black)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5, left: 70),
+                child: Text(
+                    'we are excited to help you book and manage your service appointments with ease',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 17,
+                        color: Colors.grey)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: Container(
+                  width: 300,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent[700],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+        ),
       ),
-    )
-
-
-
-
-  ],
-),
-
-
-
-      ),
-
-
-
-
     );
   }
 }
